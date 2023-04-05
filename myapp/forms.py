1e041a798e7ff792444ecf481024717c1fd92d18
dini <diniiiwl@gmail.com>
2023-04-05 12:44:31 +0700

    Added Service Report

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
-from .models import Schedule
+from .models import User, Schedule, Penerimaan, Kalibrasi, Ukes, Service
 
+#AUTH
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
@@ -19,6 +19,7 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('full_name', 'username', 'email', 'password1', 'password2', 'is_tem', 'is_tro', 'institute_name')
 
+#SCHEDULE
 class Scheduleform(forms.ModelForm):
     class Meta:
         model = Schedule
@@ -34,4 +35,110 @@ class Scheduleform(forms.ModelForm):
             'task': forms.TextInput(attrs={'class': 'form-control'}), 
             'machine': forms.TextInput(attrs={'class': 'form-control'}), 
             'location': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+#UJI PENERIMAAN
+class Penerimaanform(forms.ModelForm):
+    class Meta:
+        model = Penerimaan
+        fields = ['task_date', 'machine', 'location', 'is_passed', 'is_failed']
+        labels = {
+            'task_date': 'Scheduled Date',
+            'machine': 'Machine',
+            'location': 'Location',
+            'is_passed' : 'Passed',
+            'is_failed' : 'Failed',
+        }
+        widgets = {
+            'task_date': forms.DateInput(attrs={'class': 'form-control'}),
+            'machine': forms.TextInput(attrs={'class': 'form-control'}), 
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_passed' : forms.CheckboxInput(),
+            'is_failed' : forms.CheckboxInput(),
+        }
+
+#KALIBRASI
+class Kalibrasiform(forms.ModelForm):
+    class Meta:
+        model = Kalibrasi
+        fields = ['task_date', 'machine', 'location', 'is_passed', 'is_failed']
+        labels = {
+            'task_date': 'Scheduled Date',
+            'machine': 'Machine',
+            'location': 'Location',
+            'is_passed' : 'Passed',
+            'is_failed' : 'Failed',
+        }
+        widgets = {
+            'task_date': forms.DateInput(attrs={'class': 'form-control'}),
+            'machine': forms.TextInput(attrs={'class': 'form-control'}), 
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_passed' : forms.CheckboxInput(),
+            'is_failed' : forms.CheckboxInput(),
+        }
+
+#UKES
+class Ukesform(forms.ModelForm):
+    class Meta:
+        model = Ukes
+        fields = ['task_date', 'machine', 'location', 'is_passed', 'is_failed']
+        labels = {
+            'task_date': 'Scheduled Date',
+            'machine': 'Machine',
+            'location': 'Location',
+            'is_passed' : 'Passed',
+            'is_failed' : 'Failed',
+        }
+        widgets = {
+            'task_date': forms.DateInput(attrs={'class': 'form-control'}),
+            'machine': forms.TextInput(attrs={'class': 'form-control'}), 
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_passed' : forms.CheckboxInput(),
+            'is_failed' : forms.CheckboxInput(),
+        }
+
+#SERVICE
+class Serviceform(forms.ModelForm):
+    class Meta:
+        CHOICES = (
+        (1, 'Emergency'),
+        (2, 'Non-emergency'),
+        (3, 'Contract'),
+        (4, 'Install'),
+        (5, 'Warranty'),
+        (6, 'Other'),
+    )
+        
+        model = Service
+        fields = ['task_date', 'brand', 'type', 'serial', 'address', 'problem', 'repair', 'cust_name', 'tech_name', 'complain_num', 'is_completed', 'is_continue', 'service_type']
+        labels = {
+            'task_date': 'Service Date',
+            'brand': 'Brand',
+            'type' : 'Type',
+            'serial': 'Serial',
+            'address': 'Customer with adress',
+            'problem': 'Problem/Request',
+            'repair': 'Repair comments',
+            'cust_name': "Customer's identity",
+            'tech_name': "Technician's identity",
+            'complain_num': "Document's Number",
+            'is_completed': 'Completed',
+            'is_continue': 'Continue',
+            'service_type': "Type of service"
+        }
+
+        widgets = {
+            'task_date': forms.DateInput(attrs={'class': 'form-control'}),
+            'brand': forms.TextInput(attrs={'class': 'form-control'}), 
+            'type': forms.TextInput(attrs={'class': 'form-control'}),
+            'serial': forms.TextInput(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'problem': forms.TextInput(attrs={'class': 'form-control'}),
+            'repair': forms.TextInput(attrs={'class': 'form-control'}),
+            'cust_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'tech_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'complain_num': forms.TextInput(attrs={'class': 'form-control'}),
+            'service_type': forms.ChoiceField(choices=CHOICES),
+            'is_completed' : forms.CheckboxInput(),
+            'is_continue' : forms.CheckboxInput(),
         }
